b61bc1537e64f0fddc166234aebbd473875ecf5d
Alex <159346450+AlexandraSavenko@users.noreply.github.com>
2024-08-23 05:35:22 +0300

    icons added

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,18 @@
 import css from "./Contact.module.css";
+import { IoMdPerson } from "react-icons/io";
+import { FaPhoneAlt } from "react-icons/fa";
 export default function Contact({ contact: { name, number } }) {
   return (
     <div className={css.wrap}>
       <div>
-        <p>{name}</p>
-        <p>{number}</p>
+        <div className={css.textPlusIcon}>
+          <IoMdPerson />
+          <p>{name}</p>
+        </div>
+        <div className={css.textPlusIcon}>
+          <FaPhoneAlt className={css.noMargin} />
+          <p className={css.noMargin}>{number}</p>
+        </div>
       </div>
 
       <button className={css.button} type="button">
