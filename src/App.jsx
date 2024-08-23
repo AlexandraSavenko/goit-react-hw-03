3023918aebcd790352fb81d87eb86dacbe8df41c
Alex <159346450+AlexandraSavenko@users.noreply.github.com>
2024-08-23 07:04:42 +0300

    addNumbers added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import ContactList from "./components/ContactList/ContactList";
 import "./App.css";
 import SearchBox from "./components/SearchBox/SearchBox";
+import ContactForm from "./components/ContactForm/ContactForm";
 
 function App() {
   const [contacts, setContact] = useState([
@@ -10,6 +11,15 @@ function App() {
     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
     { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
   ]);
+  const addNumber = (newNumber) => {
+    console.log(newNumber);
+    setContact((allNumbers) => {
+      return [
+        ...allNumbers,
+        { id: "", name: newNumber.name, number: newNumber.number },
+      ];
+    });
+  };
   const [filter, setFilter] = useState("");
   const visibleNumbers = contacts.filter((contact) => {
     return contact.name.toLowerCase().includes(filter.toLowerCase());
@@ -18,6 +28,7 @@ function App() {
   return (
     <div>
       <h1>Phonebook</h1>
+      <ContactForm onAdd={addNumber} />
       <SearchBox value={filter} onFilter={setFilter} />
       <ContactList contArr={visibleNumbers} />
     </div>
